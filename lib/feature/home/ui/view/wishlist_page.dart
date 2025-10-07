832bc7840f8277d30d4ed1cac1e789879ae159ff
Ahmed Elgammal <elgammal2024@gmail.com>
2025-10-07 22:38:07 +0300

    Implement Wishlist Page UI with Navigation and Product Display
    - Created a new WishlistPage with a structured layout including a back button and edit option.
    - Integrated a grid view to display products in the wishlist using CustomProductCard.
    - Enhanced user experience with a responsive design and navigation to product details.

--- lib/feature/home/ui/view/wishlist_page.dart
+++ lib/feature/home/ui/view/wishlist_page.dart
@@ -1,10 +1,119 @@
+import 'package:e_commerce_app/core/router/routes.dart';
+import 'package:e_commerce_app/core/utils/app_colors.dart';
+import 'package:e_commerce_app/core/utils/app_images.dart';
+import 'package:e_commerce_app/core/utils/app_styles.dart';
+import 'package:e_commerce_app/feature/home/ui/view/widget/custom_product_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class WishlistPage extends StatelessWidget {
   const WishlistPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: AppColors.whiteColor,
+
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+              child: Row(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                      color: AppColors.grey3Color,
+                    ),
+                    child: Icon(Icons.arrow_back),
+                  ),
+                  const Spacer(),
+                  Container(
+                    padding: const EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(99),
+                      color: AppColors.grey3Color,
+                    ),
+                    child: SvgPicture.asset(AppImages.lockIcon),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: CustomScrollView(
+                slivers: [
+                  SliverToBoxAdapter(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Row(
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                '365 Items',
+                                style: AppStyles.styleRegular20(
+                                  context,
+                                ).copyWith(fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                'in wishlist',
+                                style: AppStyles.styleRegular15(context),
+                              ),
+                            ],
+                          ),
+                          Spacer(),
+                          Container(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 16,
+                              vertical: 8,
+                            ),
+                            decoration: BoxDecoration(
+                              color: AppColors.grey3Color,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Text(
+                              'Edit',
+                              style: AppStyles.styleRegular22(
+                                context,
+                              ).copyWith(color: AppColors.blackColor),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SliverPadding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    sliver: SliverGrid(
+                      delegate: SliverChildBuilderDelegate((context, index) {
+                        return InkWell(
+                          onTap: () {
+                            Navigator.pushNamed(
+                              context,
+                              Routes.homeDetilsRoute,
+                            );
+                          },
+                          child: CustomProductCard(index: index),
+                        );
+                      }, childCount: 10),
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            mainAxisSpacing: 12,
+                            crossAxisSpacing: 12,
+                            childAspectRatio: 0.65,
+                          ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
